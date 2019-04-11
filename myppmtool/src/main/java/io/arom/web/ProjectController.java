0735a11cceb0a8f02f7e4945518bf1b40270113f
Inamdar <inamdaro@pun-inamdaro-m.ds.ad.adp.com>
2019-04-11 15:44:57 +0530

    refactoring validation code

--- myppmtool/src/main/java/io/arom/web/ProjectController.java
+++ myppmtool/src/main/java/io/arom/web/ProjectController.java
@@ -1,21 +1,18 @@
 package io.arom.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.arom.domain.Project;
+import io.arom.services.MapValidationErrorService;
 import io.arom.services.ProjectService;
 
 @RestController
@@ -23,19 +20,15 @@ import io.arom.services.ProjectService;
 public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
+	@Autowired
+	MapValidationErrorService mapValidationErrorService;
 
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-		if (result.hasErrors()) {
-
-			Map<String, String> errorMap = new HashMap<String, String>();
-			for (FieldError error : result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errormap = mapValidationErrorService.mapvalidationService(result);
+		if (errormap != null)
+			return errormap;
 		Project project2 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project2, HttpStatus.CREATED);
 	}
-
 }
