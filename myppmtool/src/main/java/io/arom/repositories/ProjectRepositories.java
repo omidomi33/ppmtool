bf1a7e16c9f77ea05a1ea67e56a7a00f8092f4dd
Inamdar <inamdaro@pun-inamdaro-m.ds.ad.adp.com>
2019-04-11 12:27:30 +0530

    lect-02 create repo

---
+++ myppmtool/src/main/java/io/arom/repositories/ProjectRepositories.java
@@ -0,0 +1,11 @@
+package io.arom.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.arom.domain.Project;
+
+@Repository
+public interface ProjectRepositories extends CrudRepository<Project, Long> {
+
+}
