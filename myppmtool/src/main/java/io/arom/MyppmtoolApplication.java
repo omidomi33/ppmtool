ac4914db600d6ace10ed93412d5fb99807932519
Inamdar <inamdaro@pun-inamdaro-m.ds.ad.adp.com>
2019-04-11 11:02:07 +0530

    first commit

---
+++ myppmtool/src/main/java/io/arom/MyppmtoolApplication.java
@@ -0,0 +1,13 @@
+package io.arom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyppmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyppmtoolApplication.class, args);
+	}
+
+}
