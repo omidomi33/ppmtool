77429a8269c18726483bc376e0b0c9e8306f3706
Inamdar <inamdaro@pun-inamdaro-m.ds.ad.adp.com>
2019-04-17 14:17:18 +0530

    style and dashboard complete

--- myppm-tool-react-client/src/App.js
+++ myppm-tool-react-client/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header></Header>
         <Dashboard />
       </div>
     );
--- myppm-tool-react-client/src/components/Dashboard.js
+++ myppm-tool-react-client/src/components/Dashboard.js
@@ -4,12 +4,24 @@ import ProjectItem from './Project/ProjectItem';
 class Dashboard extends Component {
   render() {
     return (
-        <div>
-        <h1> Welcome to Dashboard</h1>
-        <ProjectItem/>
-        <ProjectItem/>
-        <ProjectItem/>
-        </div>
+      <div className="projects">
+      <div className="container">
+          <div className="row">
+              <div className="col-md-12">
+                  <h1 className="display-4 text-center">Projects</h1>
+                  <br />
+                  <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                      Create a Project
+                  </a>
+                  <br />
+                  <hr />
+
+                <ProjectItem></ProjectItem>
+              </div>
+          </div>
+      </div>
+  </div>
+
     )
   }
 }
