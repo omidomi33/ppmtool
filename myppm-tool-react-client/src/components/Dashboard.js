28b60995b6e0dd00fe2d896db074e3e2fc6588cf
Inamdar <inamdaro@pun-inamdaro-m.ds.ad.adp.com>
2019-04-17 12:21:04 +0530

    ui: project item component

--- myppm-tool-react-client/src/components/Dashboard.js
+++ myppm-tool-react-client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
   render() {
     return (
+        <div>
         <h1> Welcome to Dashboard</h1>
+        <ProjectItem/>
+        <ProjectItem/>
+        <ProjectItem/>
+        </div>
     )
   }
 }
